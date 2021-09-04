e6c3f07bcf544f40b9d9b77cab4606a4fd7b97f1
Maksim Spirin <berezdon@yandex.ru>
2021-09-04 17:07:04 +0300

    feat: Проектная работа 15, третья отправка

--- frontend/src/auth.js
+++ frontend/src/auth.js
@@ -1,5 +1,5 @@
-//export const BASE_URL = 'https://api.berezdon.nomoredomain.nomoredomains.club';
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'https://api.berezdon.nomoredomain.nomoredomains.club';
+//export const BASE_URL = 'http://localhost:3000';
 
 export const register = ( password, email) => {
   return fetch(`${BASE_URL}/signup`, {
